a2b878e428890cc8daa3dbbf8fc40031dca5a05a
Hsinha11 <156214180+Hsinha11@users.noreply.github.com>
2024-05-14 12:01:29 +0530

    Time: 411 ms (93.22%) | Memory: 34.3 MB (88.24%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
@@ -2,14 +2,9 @@ class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         c=0
         n=len(arr)
-        if n==2:return 1
-        fr=0
         d={}
         for i in arr:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
+            d[i] = 1 + d.get(i,0)
         sum = 0
         val = sorted(d.values(),reverse=True)
         for v in val:
