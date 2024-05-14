e4fd69243cd63e1eacc6f3b530a3ca11ec2dda31
Hsinha11 <156214180+Hsinha11@users.noreply.github.com>
2024-05-14 12:00:44 +0530

    Time: 416 ms (89.03%) | Memory: 34.6 MB (66.90%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
@@ -1,27 +1,19 @@
-from typing import List
-
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        n = len(arr)
-        if n == 2:
-            return 1
-        
-        # Count frequencies of elements
-        freq_map = {}
-        for num in arr:
-            freq_map[num] = freq_map.get(num, 0) + 1
-        
-        # Sort frequencies in descending order
-        sorted_freq = sorted(freq_map.values(), reverse=True)
-        
-        # Find minimum set size
-        half_n = n // 2
-        count = 0
-        sum_freq = 0
-        for freq in sorted_freq:
-            sum_freq += freq
-            count += 1
-            if sum_freq >= half_n:
-                break
-        
-        return count
+        c=0
+        n=len(arr)
+        if n==2:return 1
+        fr=0
+        d={}
+        for i in arr:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        sum = 0
+        val = sorted(d.values(),reverse=True)
+        for v in val:
+            sum+=v
+            c+=1
+            if sum>=n//2:break
+        return c
