df816b4808ad184af69981637bb5e0970fd93362
Hsinha11 <156214180+Hsinha11@users.noreply.github.com>
2024-08-04 10:56:18 +0530

    Time: 202 ms (90.71%) | Memory: 39.9 MB (51.33%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,26 +1,11 @@
 class Solution:
-    
-
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        mod = 10**9 + 7
-
-        # Generate all possible subarray sums
-        subarray_sums = []
+        l=[]
+        mod = 10**9 +7
         for i in range(n):
             s = 0
             for j in range(i, n):
                 s += nums[j]
-                subarray_sums.append(s)
-
-        # Find the sum of elements between 'left' and 'right' using heap
-        # Use a min-heap to find the kth smallest element efficiently
-        heapq.heapify(subarray_sums)
-        result = 0
-
-        for i in range(1, right + 1):
-            val = heapq.heappop(subarray_sums)
-            if i >= left:
-                result += val
-                result %= mod
-
-        return result
+                l.append(s)
+        l.sort()
+        return sum(l[left-1:right])%mod
