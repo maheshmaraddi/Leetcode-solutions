8e8a3f9427dad81155121dbebd6079099dd3a2e4
pipinstallaadit <34955763+pipinstallaadit@users.noreply.github.com>
2024-10-23 22:59:37 +0530

    Update best-time-to-buy-and-sell-stock-ii.py
    More effecient approach
    Runtime 3ms beats 96.95%
    Memory 12.66MB beats 77.46%

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,24 +1,12 @@
 class Solution:
-    def maxProfit(self, Arr: List[int]) -> int:
-        n=len(Arr)
-        dp = [[-1 for _ in range(2)] for _ in range(n + 1)]
-    
-    # Base condition: Initialize the last row of DP table to 0 since there are no more days to trade
-        dp[n][0] = dp[n][1] = 0
-        
-        for ind in range(n - 1, -1, -1):
-            for buy in range(2):
-                profit = 0
-                
-                if buy == 0:
-                    # We can buy the stock
-                    profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1])
-                elif buy == 1:
-                    # We can sell the stock
-                    profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0])
-                
-                dp[ind][buy] = profit  # Store the result in the DP table
-        
-        return dp[0][0]
+    def maxProfit(self, prices):
+        max_profit = 0
+        # Traverse the prices array
+        for i in range(1, len(prices)):
+            # Add the profit if there's a price increase from day i-1 to day i
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        return max_profit
+
                 
         
